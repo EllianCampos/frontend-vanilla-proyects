f8efa2609e8ae1dd936f9c4a564e46d1f2522e96
Ellian Campos <ellian.campos12@gmail.com>
2023-10-24 22:04:56 -0600

    v1

--- FunctionalProgramingWallet/scripts.js
+++ FunctionalProgramingWallet/scripts.js
@@ -1,62 +1,96 @@
-const formTransac = document.getElementById('formTransac')
-const tbody = document.getElementById('tbody')
+const saveTransactions = (transactions) => {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+};
 
-formTransac.addEventListener('submit',(e) => {
-    e.preventDefault()
-    addTransaction()
-})
+const getTransactions = () => {
+  return JSON.parse(localStorage.getItem("transactions")) || [];
+};
 
-const addTransaction = () => {
-    const amount = document.getElementById('amount').value
-    const type = document.getElementById('type').value
-
-    const transactions = JSON.parse(localStorage.getItem('transactions')) || []
-
-    // Añadir al final del array
-    const newTransaction = { amount, type}
-    transactions.push(newTransaction)
-
-    // Gurdar 
-    localStorage.setItem("transactions", JSON.stringify(transactions))
-    formTransac.reset()
-    showHistory()
-    showBalance()
+const moneyFormat = (amount) => {
+  const formatter = new Intl.NumberFormat('es-CR', {
+    style: 'currency',
+    minimumFractionDigits: 2,
+    currency: "CRC"
+  }) 
+  return formatter.format(amount)
 }
 
 const showBalance = () => {
-    const balance = document.getElementById('balance');
-    const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+  const balance = document.getElementById("balance");
+  const transactions = getTransactions()
 
-    const calculateBalance = (balanceAmount, transaction) => {
-        if (transaction.type === 'Retiro')
-            return balanceAmount - parseFloat(transaction.amount);
-        else if (transaction.type === 'Ingreso') 
-            return balanceAmount + parseFloat(transaction.amount);
-        else
-            return balanceAmount;
-    };
+  const calculateBalance = (balanceAmount, transaction) => {
+    if (transaction.type === "Retiro")
+      return balanceAmount - parseFloat(transaction.amount);
+    else if (transaction.type === "Ingreso")
+      return balanceAmount + parseFloat(transaction.amount);
+    else return balanceAmount;
+  };
 
-    const balanceAmount = transactions.reduce(calculateBalance, 0);
-    balance.textContent = `₡${balanceAmount}`;
-}
+  const balanceAmount = transactions.reduce(calculateBalance, 0);
+  balance.textContent = `${moneyFormat(balanceAmount)}`;
+};
 
 const showHistory = () => {
-    const transactions = JSON.parse(localStorage.getItem('transactions')) || []
-    const transactionsOrdered = transactions.reverse()
+	const tbody = document.getElementById("tbody");
+  const transactions = getTransactions()
+  const transactionsOrdered = transactions.reverse();
 
-    // Limpiar la tabla   
-    while(tbody.hasChildNodes())
-	    tbody.removeChild(tbody.firstChild);
-    
-    for (const transaction of transactions) {
-        tbody.innerHTML += `
-            <tr class="${transaction.type=="Retiro"? "table-danger":"table-light"}">
-                <td>${transaction.type}</td>
-                <td>${transaction.amount}</td>
-            </tr>
-        `
-    }
-}
+  // Limpiar la tabla
+  while (tbody.hasChildNodes()) tbody.removeChild(tbody.firstChild);
 
-showHistory()
-showBalance()
+	// Crear las nuevas filas
+  const rows = transactionsOrdered.map((transaction) => {
+    return `
+        <tr class=${
+          transaction.type == "Retiro" ? "table-danger" : "table-light"
+        }>
+            <td>${transaction.type}</td>
+            <td>${moneyFormat(transaction.amount)}</td>
+        </tr>
+    `;
+  });
+
+  tbody.innerHTML = rows.join("");
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  showHistory();
+  showBalance();
+});
+
+document.getElementById("formTransac").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const amount = document.getElementById("amount");
+  const type = document.getElementById("type");
+  const alertbox = document.getElementById("alertbox");
+
+  // Validar el campo de texto
+  if (amount.value.trim() === "") {
+    alertbox.classList.remove("d-none");
+    alertbox.textContent = "Ingresa el monto";
+    console.log("1");
+    return;
+  } else if (isNaN(amount.value.trim())) {
+    alertbox.classList.remove("d-none");
+    alertbox.textContent = "El monto debe ser númerico";
+    console.log("2");
+    return;
+  }
+  alertbox.classList.add("d-none");
+  alertbox.textContent = "";
+
+  // Obtener las transacciones existentes
+  const transactions = getTransactions();
+
+  // Añadir al final del array
+  const newTransaction = { amount: amount.value, type: type.value };
+  transactions.push(newTransaction);
+
+  // Gurdar
+	saveTransactions(transactions)
+  formTransac.reset();
+  showHistory();
+  showBalance();
+});
